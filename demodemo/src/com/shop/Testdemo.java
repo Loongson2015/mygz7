1eab55a73046523f8e70000b5763a7cd5a063756
李四 <66666@qq.com>
2019-11-12 10:46:36 +0800

    这是我的第二次

--- demodemo/src/com/shop/Testdemo.java
+++ demodemo/src/com/shop/Testdemo.java
@@ -3,5 +3,6 @@ package com.shop;
 public class Testdemo {
     public static void main(String[] args) {
         System.out.println("abafdddddddddddddddddddddddddddd");
+        System.out.println("cccccccccccccccccccccccccc");
     }
 }
